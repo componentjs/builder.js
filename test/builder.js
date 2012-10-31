97382e8f25eca85ae29b7819a66b127f504826fe
TJ Holowaychuk <tj@vision-media.ca>
2012-10-31 14:45:41 -0700

    add lookup paths to fix tests

--- test/builder.js
+++ test/builder.js
@@ -11,6 +11,7 @@ describe('Builder', function(){
   describe('.buildScripts(fn)', function(){
     it('should build the scripts', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.buildScripts(function(err, js){
         if (err) return done(err);
         var out = read('test/fixtures/hello-js.js', 'utf8');
@@ -21,6 +22,7 @@ describe('Builder', function(){
 
     it('should buffer components only once', function(done){
       var builder = new Builder('test/fixtures/boot');
+      builder.addLookup('test/fixtures');
       builder.buildScripts(function(err, js){
         if (err) return done(err);
         var out = read('test/fixtures/ignore.js', 'utf8');
@@ -31,6 +33,7 @@ describe('Builder', function(){
 
     it('should emit "dependency" events', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.buildScripts(function(){});
       builder.on('dependency', function(builder){
         builder.dir.should.be.a('string');
@@ -43,6 +46,7 @@ describe('Builder', function(){
   describe('.buildStyles(fn)', function(){
     it('should build the styles', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.buildStyles(function(err, css){
         if (err) return done(err);
         var out = read('test/fixtures/hello.css', 'utf8');
@@ -53,6 +57,7 @@ describe('Builder', function(){
 
     it('should emit "dependency" events', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.buildStyles(function(){});
       builder.on('dependency', function(builder){
         builder.dir.should.be.a('string');
@@ -65,6 +70,7 @@ describe('Builder', function(){
   describe('.build(fn)', function(){
     it('should build js', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.build(function(err, res){
         if (err) return done(err);
         var out = read('test/fixtures/hello.js', 'utf8');
@@ -75,6 +81,7 @@ describe('Builder', function(){
 
     it('should build css', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.build(function(err, res){
         if (err) return done(err);
         var out = read('test/fixtures/hello.css', 'utf8');
@@ -85,6 +92,7 @@ describe('Builder', function(){
 
     it('should load the require.js script', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.build(function(err, res){
         if (err) return done(err);
         var out = require('component-require');
@@ -96,6 +104,7 @@ describe('Builder', function(){
 
   it('should build local dependencies', function(done){
     var builder = new Builder('test/fixtures/bundled');
+    builder.addLookup('test/fixtures');
     builder.addLookup('test/fixtures/lib/components');
     builder.build(function(err, res){
       if (err) return done(err);
@@ -114,6 +123,7 @@ describe('Builder', function(){
 
   it('should not build development dependencies by default', function(done){
     var builder = new Builder('test/fixtures/dev-deps');
+    builder.addLookup('test/fixtures');
     builder.build(function(err, res){
       if (err) return done(err);
       res.js.should.include('component-emitter/index.js');
@@ -147,7 +157,7 @@ describe('Builder', function(){
     
     it('can build scripts for dependencies of dependencies...', function(done){
       var builder = new Builder('test/fixtures/lookups/deep3');
-      builder.addLookup(['test/fixtures', 'examples/components']);
+      builder.addLookup(['test/fixtures', 'test/fixtures/lookups', 'examples/components']);
       builder.build(function(err, res){
         if (err) return done(err);
         var out = read('test/fixtures/lookups-deep3-js.js', 'utf8');
@@ -160,6 +170,7 @@ describe('Builder', function(){
   describe('.development()', function(){
     it('should build development dependencies', function(done){
       var builder = new Builder('test/fixtures/dev-deps');
+      builder.addLookup('test/fixtures');
       builder.development();
       builder.build(function(err, res){
         if (err) return done(err);
@@ -173,6 +184,7 @@ describe('Builder', function(){
   describe('.ignore(name)', function(){
     it('should ignore the given component and its deps', function(done){
       var builder = new Builder('test/fixtures/hello');
+      builder.addLookup('test/fixtures');
       builder.ignore('component/emitter');
       builder.build(function(err, res){
         if (err) return done(err);
@@ -185,6 +197,7 @@ describe('Builder', function(){
 
   it('should support "main"', function(done){
     var builder = new Builder('test/fixtures/main-boot');
+    builder.addLookup('test/fixtures');
     builder.build(function(err, res){
       if (err) return done(err);
       res.js.should.include('require.alias("boot/boot.js", "boot/index.js")');
